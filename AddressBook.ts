85888030d55f982d37f535795d34e2019aa57898
Bassem <bassem.magdyy94@gmail.com>
2021-12-14 16:32:36 +0200

    add feature blacklist whitelist contacts
    add unit tests

--- AddressBook.ts
+++ AddressBook.ts
@@ -1,3 +1,18 @@
+/*
+ * (C) Symbol Contributors 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
 import {IContact} from "./IContact";
 
 export class AddressBook {
@@ -18,6 +33,20 @@ export class AddressBook {
      */
     getAllContacts() {
         return Object.values(this.contacts);
+    }
+
+    /**
+     * Get blacklisted contacts by a given address
+     */
+    getBlackListedContacts() {
+        return Object.values(this.contacts).filter(contact=>contact.isBlackListed);
+    }
+
+    /**
+     * Get whitelisted contacts by a given address
+     */
+    getWhiteListedContacts() {
+        return Object.values(this.contacts).filter(contact=>!contact.isBlackListed);
     }
 
     /**
